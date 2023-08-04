a1eae065fe3fb0176681a95cc2318d124a3b4c58
hallucinogenizer <22100063@lums.edu.pk>
2023-08-05 02:02:12 +0500

    chakra-ui navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import Header from "@/components/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Header />
+      <div className="px-6 py-4">
+        <Component {...pageProps} />
+      </div>
+    </ChakraProvider>
+  );
 }
